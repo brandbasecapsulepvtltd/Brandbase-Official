dcd9b2f9169a040e5d660d0df7abc6ff2eb57663
vinayak andhere <vinayakandhere4@gmail.com>
2025-12-19 18:25:11 +0530

    AVproduction

--- Website/client/src/components/ServiceCategory/AVproduction/VideoProductionHero.jsx
+++ Website/client/src/components/ServiceCategory/AVproduction/VideoProductionHero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const VideoProductionHero = () => {
   return (
-    <section className="relative h-screen w-full overflow-hidden flex items-center mt-5">
+    <section className="relative h-screen w-full overflow-hidden flex items-center mt-6">
       {/* Background Video */}
       <video
         autoPlay
