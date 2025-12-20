23ccd40adbf46972a9b847e78d07ff62b5ba8e44
vinayak andhere <vinayakandhere4@gmail.com>
2025-12-20 13:05:55 +0530

    pass key changed

--- Website/backend/utils/emailService.js
+++ Website/backend/utils/emailService.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: "vinayak.brandbasecapsule@gmail.com",
+    pass: "zyjd rgry rvch dsax"
   }
 });
 
