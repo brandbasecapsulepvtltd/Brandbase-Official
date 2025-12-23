fb76a13d5d8b7fa1e93befa4a41876af0502eb9a
vinayak andhere <vinayakandhere4@gmail.com>
2025-12-23 13:29:32 +0530

    contact us final update i guess

--- Website/client/src/components/Contact/ContactMap/demo.jsx
+++ Website/client/src/components/Contact/ContactMap/demo.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 export default function MapDemo() {
   return (
-    <div className="py-40 dark:bg-black bg-white w-full">
+    <div className="py-40 bg-white w-full">
       <div className="max-w-7xl mx-auto text-center">
         <p className="font-bold text-xl md:text-4xl dark:text-white text-black">
           Global{" "}
